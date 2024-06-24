be47cf8f5b4a7d65e477b35501c8aca0e9a30395
michele laptop <micheledong2803@gmail.com>
2024-06-24 17:54:39 +0200

    m2 - card

--- data.php
+++ data.php
@@ -4,13 +4,20 @@ include __DIR__ . '/models/Cibo.php';
 include __DIR__ . '/models/Gioco.php';
 include __DIR__ . '/models/Cuccia.php';
 
-$pallinaGatto = new Gioco("pallina","gatto",8.99, 0.3);
-$pallinaCane = new Gioco("pallina","cane",8.99, 0.3);
+// $pallinaGatto = new Gioco("pallina","gatto",8.99, 0.3);
+// $pallinaCane = new Gioco("pallina","cane",8.99, 0.3);
 
-$cucciaGatto = new Cuccia("cuccia","gatto",19.99, 0.2);
-$cucciaCane = new Cuccia("cuccia","cane",19.99, 0.2);
+// $cucciaGatto = new Cuccia("cuccia","gatto",19.99, 0.2);
+// $cucciaCane = new Cuccia("cuccia","cane",19.99, 0.2);
 
-$ciboGatto = new Cibo("cibo","gatto",10.99, 0.1);
-$ciboCane = new Cibo("cibo","cane",10.99, 0.1);
+// $ciboGatto = new Cibo("cibo","gatto",10.99, 0.1);
+// $ciboCane = new Cibo("cibo","cane",10.99, 0.1);
 
-
+$prodotti = [
+    new Gioco("Pallina","gatto",8.99, 0.3),
+    new Gioco("Pallina","cane",8.99, 0.3),
+    new Cuccia("Cuccia","gatto",19.99, 0.2),
+    new Cuccia("Cuccia","cane",19.99, 0.2),
+    new Cibo("Cibo","gatto",10.99, 0.1),
+    new Cibo("Cibo","cane",10.99, 0.1)
+];
--- index.php
+++ index.php
@@ -1,16 +1,50 @@
-<?php 
-include __DIR__ . "/data.php" 
+<?php
+include __DIR__ . "/data.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <?php
-        echo number_format($ciboGatto->getPrice(),2,",","");
-    ?>
+    <div class="d-flex">
+        <?php
+        foreach ($prodotti as $prodotto) {
+            echo
+            '<div class="card">
+            
+                <div class="productName">Nome: '
+                    .
+                    $prodotto->name
+                    .
+                '</div>
+            
+                <div class="productAnimalCategory">Animale: '
+                    .
+                    $prodotto->category
+                    .
+                '</div>
+            
+                <div class="productCategory">Categoria: '
+                    .
+                    get_class($prodotto)
+                    .
+                '</div>
+
+                <div class="price">Prezzo: '
+                    .
+                    number_format($prodotto->getPrice(),2,",","")
+                    .
+                '</div>
+            </div>';
+        }
+        ?>
+    </div>
 </body>
+
 </html>
